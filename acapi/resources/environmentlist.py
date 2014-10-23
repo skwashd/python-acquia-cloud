62b71501898e49053c3c83d203406c12496b9e6f
Dave Hall <dave.hall@skwashd.com>
2014-10-24 01:55:07 +1100

    Override environment URI generator logic

--- acapi/resources/environmentlist.py
+++ acapi/resources/environmentlist.py
@@ -7,6 +7,21 @@ class EnvironmentList(AcquiaList):
 
     """Dict of Acquia Cloud API Server resources keyed by hostname."""
 
+    def get_resource_uri(self, name):
+        """ Generate the resource URI.
+
+        Parameters
+        ----------
+        name : str
+            The name of the environment resource.
+
+        Returns
+        -------
+        str
+            The resource URI.
+        """
+        return '{base_uri}/{name}'.format(base_uri=self.uri, name=name)
+
     def set_base_uri(self, base_uri):
         """ Set the base URI for server resources.
 
