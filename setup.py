1858b61dfc9a3582e6ceca3e88bee5ed0c80fb7b
Dave Hall <skwashd@gmail.com>
2017-02-06 02:19:10 +1100

    Bump the version for the 0.4.3 release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name='acapi',
-    version='0.4.2',
+    version='0.4.3',
     description='Acquia Cloud API client.',
     author='Dave Hall',
     author_email='me@davehall.com.au',
